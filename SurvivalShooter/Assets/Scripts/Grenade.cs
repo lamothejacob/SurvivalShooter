b15af721e6383579eda8a249b176be28bd51a8b3
GwilymFS <132724902+GwilymFS@users.noreply.github.com>
2023-05-23 20:09:21 -0700

    Grenades now have an arc
    Added a variable to control the grenades arc

--- SurvivalShooter/Assets/Scripts/Grenade.cs
+++ SurvivalShooter/Assets/Scripts/Grenade.cs
@@ -9,11 +9,13 @@ public class Grenade : MonoBehaviour
     [SerializeField] Rigidbody rb;
     [SerializeField] int timer;
     [SerializeField] int speed;
+    [SerializeField] int curve;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        rb.velocity = Camera.main.transform.forward * speed;
+        rb.velocity = (Camera.main.transform.forward * speed);
+        rb.velocity += new Vector3(0, curve, 0);
 
         yield return new WaitForSeconds(timer);
 
