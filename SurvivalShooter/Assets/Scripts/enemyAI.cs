6446e434e21ee4cddb55ad32f8b6cb300fd4cc88
GwilymFS <132724902+GwilymFS@users.noreply.github.com>
2023-05-24 23:06:12 -0700

    Enemy animation on taking damage

--- SurvivalShooter/Assets/Scripts/enemyAI.cs
+++ SurvivalShooter/Assets/Scripts/enemyAI.cs
@@ -136,6 +136,7 @@ public class enemyAI : MonoBehaviour, IDamage, IPhysics
         }
         else
         {
+            anim.SetTrigger("Damage");
             StartCoroutine(flashColor());
         }
     }
