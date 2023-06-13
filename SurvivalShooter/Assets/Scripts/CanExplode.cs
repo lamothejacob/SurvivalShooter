ae9cc41a079b9d2e21a19704fa73e016dc755acb
GwilymFS <132724902+GwilymFS@users.noreply.github.com>
2023-06-13 15:57:47 -0700

    Destruction level updates
    Explosion now effect explosive items

--- SurvivalShooter/Assets/Scripts/CanExplode.cs
+++ SurvivalShooter/Assets/Scripts/CanExplode.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanExplode : MonoBehaviour, IDamage
+public class CanExplode : MonoBehaviour, IDamage, IPhysics
 {
     [SerializeField] int HP;
     [SerializeField] GameObject explosion;
@@ -10,7 +10,17 @@ public class CanExplode : MonoBehaviour, IDamage
 
     public void TakeDamage(int amount)
     {
-        HP -= amount;
+        explode(amount);
+    }
+
+    public void takePushBack(Vector3 direc, int damage)
+    {
+        explode(damage);
+    }
+
+    void explode(int damage)
+    {
+        HP -= damage;
         if (HP <= 0)
         {
             Instantiate(explosion, transform.position, transform.rotation);
